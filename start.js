d54ee042540ca77dc3a0213fd449b09dc21e67cf
KaraKartal <k-a-r-a-kartal@hotmail.de>
2018-10-14 17:27:32 +0200

    update

--- start.js
+++ start.js
@@ -29831,7 +29831,7 @@
                   height: 148,
                   numCols: 2,
                   // location of our backend
-                  action: 'api/update.php',
+                  action: 'api/update/update.php',
                   fields: [{
                   type: "RowSpacer",
                           height: 10
